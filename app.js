e947228f25e3da834360228a93196614282d60dd
raolis1989 <raolis1989@gmail.com>
2023-06-30 13:56:20 -0400

    generation jwt

--- app.js
+++ app.js
@@ -3,3 +3,37 @@ console.log('Hello, Demo!')
 var moment = require('moment');
 var date = moment().format('LL');
 console.log(date);
+
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+let duration = 25 * 365.25 * 24 * 60 * 60;
+const secret =' -----BEGIN RSA PRIVATE KEY-----MIIBOQIBAAJBAJjHqtJHB748Wvv3LxULWtPS0wk9BpMKIpjmCTt0m4cjVJZJtNMnwXJc3PunOpso0U6pOHyRzl4vyH3amvY2Rm0CAwEAAQJAIphT3fmnjzQrhhKa3rELiBudHeJrsrAA1Y6BWK026oqCn/j6BWoBu9hXJJbfrZdO4gaAd0qz3RMAhJAdjSyoAQIhAOrnZLoS3mMfovn0Nmorxl3/V0v4GNcDX/LLKtmV73BBAiEApoAvvnh0TYYHQZ2f8DzBC4G8snSMGDQwnjoyaPYcyy0CIG/ekSbYk5ZrW6dkOYHopQjg8McaOWI0lj4/qYGEGUrBAiB7fyTqoCmTvs/vCV0yMb32LPqrWur2oO9WzU/KrpHY2QIgckuD5ieqhWNx2SymCzRQRQ1mQ+fhoS0/9y6UDUyQI18=-----END RSA PRIVATE KEY-----';
+
+let token;
+
+try {
+	// Generate payload
+	token = jwt.sign({ 
+		iss: 'jwt-tutorial', // Issuer (who created and signed this token)
+		sub: 'USER_ID', // Subject (whom the token refers to)
+		exp: Math.floor(Date.now() / 1000) + duration, // Token expiry date
+		roles: ['student']
+	}, secret);
+
+	console.log("-- Generated JWT --");
+	console.log('JWT :', token);
+} catch (errorOnPayload) {
+	console.log("\nFailed to generate payload! An error happened :", errorOnPayload);
+}
+
+try {
+	let decoded = jwt.decode(token, { complete: true });
+
+	console.log("\n-- Decoded JWT --");
+	console.log("Header :", decoded.header);
+	console.log("Payload :", decoded.payload);
+	console.log("Signature :", decoded.signature); // Header + Payload generate signature
+} catch (errorOnDecode) {
+	console.log("\nFailed to decode JWT! An error occurred :", errorOnDecode);
+}
